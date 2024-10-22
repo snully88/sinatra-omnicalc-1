578a6492e1ed865d84488c66bf2b57c9a09e9264
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-10-22 03:25:44 +0000

    whats wrong?

--- app.rb
+++ app.rb
@@ -22,3 +22,14 @@ get ("/square/results") do
   @the_result = @the_num ** 2
   erb(:square_results)
 end
+
+get("/square_root/new") do
+  erb(:square_root_new)
+end
+
+get("square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result_root = Math.sqrt(@the_num)
+  erb(:results_square_root)
+end
