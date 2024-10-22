12f71c9bd4cdd636ebe4c466dea8e5a8cf237ead
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-10-22 02:16:54 +0000

    uhhh

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy) do
+
+erb(:hello)
+
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
 end
